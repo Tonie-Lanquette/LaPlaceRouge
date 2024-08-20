1a41859c812c52b0840c4200a80bebad44c2b432
Elodie <elodie.grienay.simplon@gmail.com>
2024-08-20 09:12:08 +0200

    back menu

---
+++ src/Form/MenuType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dishies;
+use App\Entity\Menu;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('dishies', EntityType::class, [
+                'class' => Dishies::class,
+                'choice_label' => 'title',
+                'expanded' => true,
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
