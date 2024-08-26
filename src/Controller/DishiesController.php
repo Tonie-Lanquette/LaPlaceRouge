dbb7f3fe580ba7908bfad5eb5f9c4cef08dd7167
Tonie <tonie.lanquette.simplon@gmail.com>
2024-08-26 13:50:52 +0200

    emailing

--- src/Controller/DishiesController.php
+++ src/Controller/DishiesController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 #[Route('/dish')]
 #[IsGranted('ROLE_ADMIN')]
